d02a3bc46299c63832fb98f329a2b8b9875f7935
Jamie Lawrence <jlawrence@btflt167.beaconsfield.fresca.co.uk>
2014-05-30 16:30:29 +0100

    Testing

--- app.js
+++ app.js
@@ -208,3 +208,28 @@ function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
   res.redirect('/login')
 }
+
+app.dynamicHelpers({
+    user: function(req, res){
+        var roles, name;
+
+        if (req.session && req.session.auth == true) {
+            roles = ['member'];
+            name = (req.session.user) ? req.session.user.name : 'Registered member';
+            id = (req.session.user) ? req.session.user.id : 0;
+        }
+        else {
+            roles = ['guest'];
+            name = 'Guest';
+            id = null;
+        }
+
+        return {
+            name: name, 
+            id: id,
+            roles: roles,
+            isGuest: roles.indexOf('guest') !== -1,
+            isAdmin: roles.indexOf('admin') !== -1
+        }
+    }
+});
