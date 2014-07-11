cb2f0ed6cd66e2d5aaeebda61be8d33f96ab0fcd
Jamie Lawrence <jlawrence@BTFLT167.local>
2014-07-11 11:27:39 +0100

    Auth stuff

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://localhost/auth' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/auth' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 };
